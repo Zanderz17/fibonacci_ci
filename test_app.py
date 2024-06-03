a8ef6e7eae4b55bb1f4b99cdf2a84d132439e868
Zanderz17 <sanders.chancan@utec.pe>
2024-06-02 21:54:16 -0500

    testing coverage failure

--- test_app.py
+++ test_app.py
@@ -2,36 +2,8 @@ from fastapi.testclient import TestClient
 from app import app
 import time
 
+
 client = TestClient(app)
-
-
-def test_fibonacci_success():
-    response = client.get("/get_fibonacci/5")
-    assert response.status_code == 200
-    assert response.json() == 5
-
-
-def test_fibonacci_negative():
-    response = client.get("/get_fibonacci/-1")
-    assert response.status_code == 400
-    assert response.json() == {"detail": "El número debe ser un entero no negativo."}
-
-
-def test_fibonacci_one():
-    response = client.get("/get_fibonacci/1")
-    assert response.status_code == 200
-    assert response.json() == 1
-
-
-def test_fibonacci_twenty():
-    response = client.get("/get_fibonacci/20")
-    assert response.status_code == 200
-    assert response.json() == 6765
-
-
-def test_fibonacci_invalid_input():
-    response = client.get("/get_fibonacci/abc")
-    assert response.status_code == 422
 
 
 def test_fibonacci_performance_average():
@@ -47,4 +19,4 @@ def test_fibonacci_performance_average():
         assert response.json() is not None
 
     average_time = (total_time / number_of_requests) * 1000
-    assert average_time <= 4.5, f"Average request time too long: {average_time}ms, exceeds 4.5ms"
+    assert average_time <= 6.5, f"Average request time too long: {average_time}ms, exceeds 6.5ms"
